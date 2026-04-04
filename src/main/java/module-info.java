4a97b7c41b57bae976dd2f01cd84c6162fd41f8b
Joyanne <qiyuepoong@gmail.com>
2026-04-04 16:56:14 +1000

    build: add ikonli and java.logging dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,11 @@
 module com.example.cab302assignment {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.logging;
 
 
     opens com.example.cab302assignment to javafx.fxml;
     exports com.example.cab302assignment;
+    exports com.example.cab302assignment.controller;
+    opens com.example.cab302assignment.controller to javafx.fxml;
 }
