ca423a4c906027a4097fe9af8abc10233626711a
Zefira Hong <zefira.hong@gmail.com>
2026-04-14 19:31:31 +1000

    Add DEACTIVATED in MemberStatus enum

--- src/main/java/com/example/cab302assignment/model/MemberStatus.java
+++ src/main/java/com/example/cab302assignment/model/MemberStatus.java
@@ -2,6 +2,7 @@ package com.example.cab302assignment.model;
 
 public enum MemberStatus {
     NOT_FOUND,    // User not exists
-    NOT_A_MEMBER, // User a member yet
-    ACTIVE        // User is an active member in the org
+    NOT_A_MEMBER, // User is no a member yet
+    ACTIVE,       // User is an active member in the org
+    DEACTIVATED      // User's membership is deactivated
 }
